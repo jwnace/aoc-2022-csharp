6b13b23ddb538168b15ecfb48379beda5774f740
Joe Nace <jwnace@gmail.com>
2022-12-01 09:01:01 -0500

    refactored day 1 solution to be more readable

--- src/aoc-2022-csharp/Day01/Day01.cs
+++ src/aoc-2022-csharp/Day01/Day01.cs
@@ -4,7 +4,14 @@ public static class Day01
 {
     private static readonly string Input = File.ReadAllText("Day01/day01.txt");
 
-    public static int Part1() => Input.Split($"{Environment.NewLine}{Environment.NewLine}").Select(x => x.Split(Environment.NewLine).Select(int.Parse)).Max(x => x.Sum(y => y));
+    public static int Part1() => Input.Split($"{Environment.NewLine}{Environment.NewLine}")
+        .Select(elf => elf.Split(Environment.NewLine).Select(int.Parse))
+        .Max(calories => calories.Sum());
 
-    public static int Part2() => Input.Split($"{Environment.NewLine}{Environment.NewLine}").Select(x => x.Split(Environment.NewLine).Select(int.Parse)).Select(x => x.Sum()).OrderByDescending(x => x).Take(3).Sum();
+    public static int Part2() => Input.Split($"{Environment.NewLine}{Environment.NewLine}")
+        .Select(elf => elf.Split(Environment.NewLine).Select(int.Parse))
+        .Select(calories => calories.Sum())
+        .OrderByDescending(x => x)
+        .Take(3)
+        .Sum();
 }
