1f27e71c986a7d2656b3002d6cb8d1563ffa0837
Joe Nace <jwnace@gmail.com>
2022-12-01 00:23:01 -0500

    added unit tests for day 1

--- tests/aoc-2022-csharp-tests/Day01Tests.cs
+++ tests/aoc-2022-csharp-tests/Day01Tests.cs
@@ -1,15 +1,30 @@
-﻿namespace aoc_2022_csharp_tests;
+﻿using aoc_2022_csharp.Day01;
+
+namespace aoc_2022_csharp_tests;
 
 public class Day01Tests
 {
     [Test]
-    public void Foo()
+    public void Part1Test()
     {
         // arrange
-        var expected = true;
+        var expected = 69528;
 
         // act
-        var actual = true;
+        var actual = Day01.Part1();
+
+        // assert
+        actual.Should().Be(expected);
+    }
+
+    [Test]
+    public void Part2Test()
+    {
+        // arrange
+        var expected = 206152;
+
+        // act
+        var actual = Day01.Part2();
 
         // assert
         actual.Should().Be(expected);
