efd78b75f73019bd93d66b31a05f1891657e475a
Joe Nace <jwnace@gmail.com>
2022-12-01 00:05:14 -0500

    initial working solution for day 1

--- src/aoc-2022-csharp/Day01/Day01.cs
+++ src/aoc-2022-csharp/Day01/Day01.cs
@@ -2,9 +2,37 @@
 
 public static class Day01
 {
-    private static readonly string[] Input = File.ReadAllLines("Day01/day01.txt");
+    private static readonly string Input = File.ReadAllText("Day01/day01.txt");
 
-    public static int Part1() => 1;
+    public static int Part1()
+    {
+        var query = Input.Split("\r\n\r\n");
 
-    public static int Part2() => 2;
+        var sums = new List<int>();
+
+        foreach (var group in query)
+        {
+            var foo = group.Split("\r\n").Select(int.Parse);
+
+            sums.Add(foo.Sum());
+        }
+
+        return sums.Max();
+    }
+
+    public static int Part2()
+    {
+        var query = Input.Split("\r\n\r\n");
+
+        var sums = new List<int>();
+
+        foreach (var group in query)
+        {
+            var foo = group.Split("\r\n").Select(int.Parse);
+
+            sums.Add(foo.Sum());
+        }
+
+        return sums.OrderByDescending(x => x).Take(3).Sum();
+    }
 }
