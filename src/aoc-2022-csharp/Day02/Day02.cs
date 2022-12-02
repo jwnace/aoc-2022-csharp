ed1adc4a44389b82a77f0d623c2df9c1aa20bca2
Joe Nace <jwnace@gmail.com>
2022-12-02 00:24:28 -0500

    initial working solution for day 2

--- src/aoc-2022-csharp/Day02/Day02.cs
+++ src/aoc-2022-csharp/Day02/Day02.cs
@@ -4,7 +4,123 @@ public static class Day02
 {
     private static readonly string[] Input = File.ReadAllLines("Day02/day02.txt");
 
-    public static int Part1() => 1;
+    public static int Part1()
+    {
+        var score = 0;
 
-    public static int Part2() => 2;
+        foreach (var line in Input)
+        {
+            var values = line.Split(' ');
+            var left = values[0];
+            var right = values[1];
+
+            if (right == "X")
+            {
+                if (left == "A")
+                {
+                    score += 1 + 3;
+                }
+                else if (left == "B")
+                {
+                    score += 1 + 0;
+                }
+                else if (left == "C")
+                {
+                    score += 1 + 6;
+                }
+            }
+            else if (right == "Y")
+            {
+                if (left == "A")
+                {
+                    score += 2 + 6;
+                }
+                else if (left == "B")
+                {
+                    score += 2 + 3;
+                }
+                else if (left == "C")
+                {
+                    score += 2 + 0;
+                }
+            }
+            else if (right == "Z")
+            {
+                if (left == "A")
+                {
+                    score += 3 + 0;
+                }
+                else if (left == "B")
+                {
+                    score += 3 + 6;
+                }
+                else if (left == "C")
+                {
+                    score += 3 + 3;
+                }
+            }
+        }
+
+        return score;
+    }
+
+    public static int Part2()
+    {
+        var score = 0;
+
+        foreach (var line in Input)
+        {
+            var values = line.Split(' ');
+            var left = values[0];
+            var right = values[1];
+
+            if (left == "A")
+            {
+                if (right == "X")
+                {
+                    score += 3 + 0;
+                }
+                else if (right == "Y")
+                {
+                    score += 1 + 3;
+                }
+                else if (right == "Z")
+                {
+                    score += 2 + 6;
+                }
+            }
+            else if (left == "B")
+            {
+                if (right == "X")
+                {
+                    score += 1 + 0;
+                }
+                else if (right == "Y")
+                {
+                    score += 2 + 3;
+                }
+                else if (right == "Z")
+                {
+                    score += 3 + 6;
+                }
+            }
+            else if (left == "C")
+            {
+                if (right == "X")
+                {
+                    score += 2 + 0;
+                }
+                else if (right == "Y")
+                {
+                    score += 3 + 3;
+                }
+                else if (right == "Z")
+                {
+                    score += 1 + 6;
+                }
+            }
+        }
+
+        return score;
+    }
 }
--- tests/aoc-2022-csharp-tests/Day02Tests.cs
+++ tests/aoc-2022-csharp-tests/Day02Tests.cs
@@ -1,15 +1,30 @@
-﻿namespace aoc_2022_csharp_tests;
+﻿using aoc_2022_csharp.Day02;
+
+namespace aoc_2022_csharp_tests;
 
 public class Day02Tests
 {
     [Test]
-    public void Foo()
+    public void Part1Test()
     {
         // arrange
-        var expected = true;
+        var expected = 13009;
 
         // act
-        var actual = true;
+        var actual = Day02.Part1();
+
+        // assert
+        actual.Should().Be(expected);
+    }
+
+    [Test]
+    public void Part2Test()
+    {
+        // arrange
+        var expected = 10398;
+
+        // act
+        var actual = Day02.Part2();
 
         // assert
         actual.Should().Be(expected);
