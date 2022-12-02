a8d1a3852666fd0e94245b33cacfe45aabcd0292
Joe Nace <jwnace@gmail.com>
2022-12-02 00:47:12 -0500

    refactored to remove the need for loops and make everything expression body members

--- src/aoc-2022-csharp/Day02/Day02.cs
+++ src/aoc-2022-csharp/Day02/Day02.cs
@@ -2,65 +2,37 @@
 
 public static class Day02
 {
-    private static readonly string[] Input = File.ReadAllLines("Day02/day02.txt");
+    private static readonly IEnumerable<(string Left, string Right)> Input = File.ReadAllLines("Day02/day02.txt")
+        .Select(x => x.Split(' '))
+        .Select(x => (x[0], x[1]));
 
-    public static int Part1()
-    {
-        var totalScore = 0;
-
-        foreach (var line in Input)
+    public static int Part1() =>
+        Input.Sum(round => round.Left switch
         {
-            var values = line.Split(' ');
-            var left = values[0];
-            var right = values[1];
+            "A" when round.Right == "X" => 1 + 3,
+            "B" when round.Right == "X" => 1 + 0,
+            "C" when round.Right == "X" => 1 + 6,
+            "A" when round.Right == "Y" => 2 + 6,
+            "B" when round.Right == "Y" => 2 + 3,
+            "C" when round.Right == "Y" => 2 + 0,
+            "A" when round.Right == "Z" => 3 + 0,
+            "B" when round.Right == "Z" => 3 + 6,
+            "C" when round.Right == "Z" => 3 + 3,
+            _ => throw new ArgumentOutOfRangeException(),
+        });
 
-            var score = left switch
-            {
-                "A" when right == "X" => 1 + 3,
-                "B" when right == "X" => 1 + 0,
-                "C" when right == "X" => 1 + 6,
-                "A" when right == "Y" => 2 + 6,
-                "B" when right == "Y" => 2 + 3,
-                "C" when right == "Y" => 2 + 0,
-                "A" when right == "Z" => 3 + 0,
-                "B" when right == "Z" => 3 + 6,
-                "C" when right == "Z" => 3 + 3,
-                _ => throw new ArgumentOutOfRangeException(),
-            };
-
-            totalScore += score;
-        }
-
-        return totalScore;
-    }
-
-    public static int Part2()
-    {
-        var totalScore = 0;
-
-        foreach (var line in Input)
+    public static int Part2() =>
+        Input.Sum(line => line.Left switch
         {
-            var values = line.Split(' ');
-            var left = values[0];
-            var right = values[1];
-
-            var score = left switch
-            {
-                "A" when right == "X" => 3 + 0,
-                "B" when right == "X" => 1 + 0,
-                "C" when right == "X" => 2 + 0,
-                "A" when right == "Y" => 1 + 3,
-                "B" when right == "Y" => 2 + 3,
-                "C" when right == "Y" => 3 + 3,
-                "A" when right == "Z" => 2 + 6,
-                "B" when right == "Z" => 3 + 6,
-                "C" when right == "Z" => 1 + 6,
-                _ => throw new ArgumentOutOfRangeException(),
-            };
-
-            totalScore += score;
-        }
-
-        return totalScore;
-    }
+            "A" when line.Right == "X" => 3 + 0,
+            "B" when line.Right == "X" => 1 + 0,
+            "C" when line.Right == "X" => 2 + 0,
+            "A" when line.Right == "Y" => 1 + 3,
+            "B" when line.Right == "Y" => 2 + 3,
+            "C" when line.Right == "Y" => 3 + 3,
+            "A" when line.Right == "Z" => 2 + 6,
+            "B" when line.Right == "Z" => 3 + 6,
+            "C" when line.Right == "Z" => 1 + 6,
+            _ => throw new ArgumentOutOfRangeException(),
+        });
 }
