0c30215a39d0283b8f41e45b2bafe9d03a9f6f16
Joe Nace <jwnace@gmail.com>
2022-12-02 00:54:15 -0500

    refactored to simplify the switch expressions by matching on the tuple values directly instead of left and right separately

--- src/aoc-2022-csharp/Day02/Day02.cs
+++ src/aoc-2022-csharp/Day02/Day02.cs
@@ -2,37 +2,37 @@
 
 public static class Day02
 {
-    private static readonly IEnumerable<(string Left, string Right)> Input = File.ReadAllLines("Day02/day02.txt")
+    private static readonly IEnumerable<(string, string)> Input = File.ReadAllLines("Day02/day02.txt")
         .Select(x => x.Split(' '))
         .Select(x => (x[0], x[1]));
 
     public static int Part1() =>
-        Input.Sum(round => round.Left switch
+        Input.Sum(round => round switch
         {
-            "A" when round.Right == "X" => 1 + 3,
-            "B" when round.Right == "X" => 1 + 0,
-            "C" when round.Right == "X" => 1 + 6,
-            "A" when round.Right == "Y" => 2 + 6,
-            "B" when round.Right == "Y" => 2 + 3,
-            "C" when round.Right == "Y" => 2 + 0,
-            "A" when round.Right == "Z" => 3 + 0,
-            "B" when round.Right == "Z" => 3 + 6,
-            "C" when round.Right == "Z" => 3 + 3,
+            ("A", "X") => 1 + 3,
+            ("B", "X") => 1 + 0,
+            ("C", "X") => 1 + 6,
+            ("A", "Y") => 2 + 6,
+            ("B", "Y") => 2 + 3,
+            ("C", "Y") => 2 + 0,
+            ("A", "Z") => 3 + 0,
+            ("B", "Z") => 3 + 6,
+            ("C", "Z") => 3 + 3,
             _ => throw new ArgumentOutOfRangeException(),
         });
 
     public static int Part2() =>
-        Input.Sum(line => line.Left switch
+        Input.Sum(line => line switch
         {
-            "A" when line.Right == "X" => 3 + 0,
-            "B" when line.Right == "X" => 1 + 0,
-            "C" when line.Right == "X" => 2 + 0,
-            "A" when line.Right == "Y" => 1 + 3,
-            "B" when line.Right == "Y" => 2 + 3,
-            "C" when line.Right == "Y" => 3 + 3,
-            "A" when line.Right == "Z" => 2 + 6,
-            "B" when line.Right == "Z" => 3 + 6,
-            "C" when line.Right == "Z" => 1 + 6,
+            ("A", "X") => 3 + 0,
+            ("B", "X") => 1 + 0,
+            ("C", "X") => 2 + 0,
+            ("A", "Y") => 1 + 3,
+            ("B", "Y") => 2 + 3,
+            ("C", "Y") => 3 + 3,
+            ("A", "Z") => 2 + 6,
+            ("B", "Z") => 3 + 6,
+            ("C", "Z") => 1 + 6,
             _ => throw new ArgumentOutOfRangeException(),
         });
 }
