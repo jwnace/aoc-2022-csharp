bc3867e36a903e53fbd1fb1f66c88b3c5672a704
Joe Nace <jwnace@gmail.com>
2022-12-04 01:07:09 -0500

    refactored to remove some duplicated logic and simplify the code

--- src/aoc-2022-csharp/Day04/Day04.cs
+++ src/aoc-2022-csharp/Day04/Day04.cs
@@ -2,27 +2,23 @@
 
 public static class Day04
 {
-    private static readonly string[] Input = File.ReadAllLines("Day04/day04.txt");
+    private static readonly IEnumerable<(Range Left, Range Right)> Pairs = File.ReadAllLines("Day04/day04.txt")
+        .Select(line => line.Split(','))
+        .Select(ranges => (Range.FromString(ranges[0]), Range.FromString(ranges[1])));
 
-    public static int Part1() => Input
-        .Select(line => new { Left = line.Split(',')[0].Split('-'), Right = line.Split(',')[1].Split('-') })
-        .Select(ranges => new
-        {
-            A = int.Parse(ranges.Left[0]),
-            B = int.Parse(ranges.Left[1]),
-            C = int.Parse(ranges.Right[0]),
-            D = int.Parse(ranges.Right[1])
-        })
-        .Count(x => (x.A <= x.C && x.B >= x.D) || (x.C <= x.A && x.D >= x.B));
+    public static int Part1() => Pairs
+        .Count(ranges => ranges.Left.Contains(ranges.Right) || ranges.Right.Contains(ranges.Left));
 
-    public static int Part2() => Input
-        .Select(line => new { Left = line.Split(',')[0].Split('-'), Right = line.Split(',')[1].Split('-') })
-        .Select(ranges => new
-        {
-            A = int.Parse(ranges.Left[0]),
-            B = int.Parse(ranges.Left[1]),
-            C = int.Parse(ranges.Right[0]),
-            D = int.Parse(ranges.Right[1])
-        })
-        .Count(x => (x.A <= x.C && x.B >= x.C) || (x.C <= x.A && x.D >= x.A));
+    public static int Part2() => Pairs.Count(ranges => ranges.Left.Overlaps(ranges.Right));
+
+    private record Range(int Start, int End)
+    {
+        public static Range FromString(string input) =>
+            new(int.Parse(input.Split('-')[0]), int.Parse(input.Split('-')[1]));
+
+        public bool Contains(Range other) => Start <= other.Start && End >= other.End;
+
+        public bool Overlaps(Range other) =>
+            (Start <= other.Start && End >= other.Start) || (other.Start <= Start && other.End >= Start);
+    }
 }
