0d0f21282ff8c4b3b0aa61d60ecb7dd14b896b6a
Joe Nace <jwnace@gmail.com>
2022-12-04 00:40:30 -0500

    refactored to use LINQ and expression body members

--- src/aoc-2022-csharp/Day04/Day04.cs
+++ src/aoc-2022-csharp/Day04/Day04.cs
@@ -4,57 +4,25 @@ public static class Day04
 {
     private static readonly string[] Input = File.ReadAllLines("Day04/day04.txt");
 
-    public static int Part1()
-    {
-        var count = 0;
-
-        foreach (var line in Input)
+    public static int Part1() => Input
+        .Select(line => new { Left = line.Split(',')[0].Split('-'), Right = line.Split(',')[1].Split('-') })
+        .Select(ranges => new
         {
-            var values = line.Split(',');
-            var left = values[0];
-            var right = values[1];
+            A = int.Parse(ranges.Left[0]),
+            B = int.Parse(ranges.Left[1]),
+            C = int.Parse(ranges.Right[0]),
+            D = int.Parse(ranges.Right[1])
+        })
+        .Count(x => (x.A <= x.C && x.B >= x.D) || (x.C <= x.A && x.D >= x.B));
 
-            var foo = left.Split('-');
-            var a = int.Parse(foo[0]);
-            var b = int.Parse(foo[1]);
-
-            var bar = right.Split('-');
-            var c = int.Parse(bar[0]);
-            var d = int.Parse(bar[1]);
-
-            if ((a <= c && b >= d) || (c <= a && d >= b))
-            {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    public static int Part2()
-    {
-        var count = 0;
-
-        foreach (var line in Input)
+    public static int Part2() => Input
+        .Select(line => new { Left = line.Split(',')[0].Split('-'), Right = line.Split(',')[1].Split('-') })
+        .Select(ranges => new
         {
-            var values = line.Split(',');
-            var left = values[0];
-            var right = values[1];
-
-            var foo = left.Split('-');
-            var a = int.Parse(foo[0]);
-            var b = int.Parse(foo[1]);
-
-            var bar = right.Split('-');
-            var c = int.Parse(bar[0]);
-            var d = int.Parse(bar[1]);
-
-            if ((a <= c && b >= c) || (c <= a && d >= a))
-            {
-                count++;
-            }
-        }
-
-        return count;
-    }
+            A = int.Parse(ranges.Left[0]),
+            B = int.Parse(ranges.Left[1]),
+            C = int.Parse(ranges.Right[0]),
+            D = int.Parse(ranges.Right[1])
+        })
+        .Count(x => (x.A <= x.C && x.B >= x.C) || (x.C <= x.A && x.D >= x.A));
 }
