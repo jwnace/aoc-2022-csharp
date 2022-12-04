9a6052e5812bf26006172026cab0d228939d9fe0
Joe Nace <jwnace@gmail.com>
2022-12-04 01:29:57 -0500

    added placeholder for day 5 solution

---
+++ src/aoc-2022-csharp/Day05/Day05.cs
@@ -0,0 +1,10 @@
+﻿namespace aoc_2022_csharp.Day05;
+
+public static class Day05
+{
+    private static readonly string[] Input = File.ReadAllLines("Day05/day05.txt");
+
+    public static int Part1() => 1;
+
+    public static int Part2() => 2;
+}
