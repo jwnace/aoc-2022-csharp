f18e551887bc63c3c14c5e85083bc23c784a7af3
Joe Nace <jwnace@gmail.com>
2022-12-04 00:06:34 -0500

    initial working solution for day 4

--- src/aoc-2022-csharp/Day04/Day04.cs
+++ src/aoc-2022-csharp/Day04/Day04.cs
@@ -4,7 +4,57 @@ public static class Day04
 {
     private static readonly string[] Input = File.ReadAllLines("Day04/day04.txt");
 
-    public static int Part1() => 1;
+    public static int Part1()
+    {
+        var count = 0;
 
-    public static int Part2() => 2;
+        foreach (var line in Input)
+        {
+            var values = line.Split(',');
+            var left = values[0];
+            var right = values[1];
+
+            var foo = left.Split('-');
+            var a = int.Parse(foo[0]);
+            var b = int.Parse(foo[1]);
+
+            var bar = right.Split('-');
+            var c = int.Parse(bar[0]);
+            var d = int.Parse(bar[1]);
+
+            if ((a <= c && b >= d) || (c <= a && d >= b))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static int Part2()
+    {
+        var count = 0;
+
+        foreach (var line in Input)
+        {
+            var values = line.Split(',');
+            var left = values[0];
+            var right = values[1];
+
+            var foo = left.Split('-');
+            var a = int.Parse(foo[0]);
+            var b = int.Parse(foo[1]);
+
+            var bar = right.Split('-');
+            var c = int.Parse(bar[0]);
+            var d = int.Parse(bar[1]);
+
+            if ((a <= c && b >= c) || (c <= a && d >= a))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
