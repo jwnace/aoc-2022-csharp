2b8493d23f70ef7c735c30d6cdaaacc2025ef3b5
Joe Nace <jwnace@gmail.com>
2022-12-05 01:32:07 -0500

    moved BuildStacks() method above Part1() and Part2()

--- src/aoc-2022-csharp/Day05/Day05.cs
+++ src/aoc-2022-csharp/Day05/Day05.cs
@@ -29,6 +29,29 @@ public static class Day05
             .Select(Instruction.Parse)
             .ToArray();
 
+    private static List<Stack<char>> BuildStacks()
+    {
+        var stacks = new List<Stack<char>> { new(), new(), new(), new(), new(), new(), new(), new(), new() };
+
+        foreach (var line in StackStrings)
+        {
+            var (a, b, c, d, e, f, g, h, i) =
+                (line[1], line[5], line[9], line[13], line[17], line[21], line[25], line[29], line[33]);
+
+            if (a != ' ') stacks[0].Push(a);
+            if (b != ' ') stacks[1].Push(b);
+            if (c != ' ') stacks[2].Push(c);
+            if (d != ' ') stacks[3].Push(d);
+            if (e != ' ') stacks[4].Push(e);
+            if (f != ' ') stacks[5].Push(f);
+            if (g != ' ') stacks[6].Push(g);
+            if (h != ' ') stacks[7].Push(h);
+            if (i != ' ') stacks[8].Push(i);
+        }
+
+        return stacks;
+    }
+
     public static string Part1()
     {
         var stacks = BuildStacks();
@@ -64,28 +87,5 @@ public static class Day05
         }
 
         return stacks.Aggregate("", (result, stack) => result + (stack.Count > 0 ? stack.Peek() : ' '));
-    }
-
-    private static List<Stack<char>> BuildStacks()
-    {
-        var stacks = new List<Stack<char>> { new(), new(), new(), new(), new(), new(), new(), new(), new() };
-
-        foreach (var line in StackStrings)
-        {
-            var (a, b, c, d, e, f, g, h, i) =
-                (line[1], line[5], line[9], line[13], line[17], line[21], line[25], line[29], line[33]);
-
-            if (a != ' ') stacks[0].Push(a);
-            if (b != ' ') stacks[1].Push(b);
-            if (c != ' ') stacks[2].Push(c);
-            if (d != ' ') stacks[3].Push(d);
-            if (e != ' ') stacks[4].Push(e);
-            if (f != ' ') stacks[5].Push(f);
-            if (g != ' ') stacks[6].Push(g);
-            if (h != ' ') stacks[7].Push(h);
-            if (i != ' ') stacks[8].Push(i);
-        }
-
-        return stacks;
     }
 }
