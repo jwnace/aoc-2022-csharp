4c71b8e7201b9bf135191a5542b35014458cc58e
Joe Nace <jwnace@gmail.com>
2022-12-06 00:23:34 -0500

    added solution for day 6

--- src/aoc-2022-csharp/Day06/Day06.cs
+++ src/aoc-2022-csharp/Day06/Day06.cs
@@ -1,10 +1,27 @@
-﻿namespace aoc_2022_csharp.Day06;
+﻿using aoc_2022_csharp.Extensions;
+
+namespace aoc_2022_csharp.Day06;
 
 public static class Day06
 {
-    private static readonly string[] Input = File.ReadAllLines("Day06/day06.txt");
+    private static readonly string Input = File.ReadAllText("Day06/day06.txt");
 
-    public static int Part1() => 1;
+    public static int Part1() => GetStartOfPacketMarker(Input, 4);
 
-    public static int Part2() => 2;
+    public static int Part2() => GetStartOfPacketMarker(Input, 14);
+
+    public static int GetStartOfPacketMarker(string message, int sliceSize)
+    {
+        var slices = message.Slice(sliceSize).ToArray();
+
+        for (var i = 0; i < slices.Length; i++)
+        {
+            if (slices[i].Distinct().Count() == sliceSize)
+            {
+                return i + sliceSize;
+            }
+        }
+
+        return 0;
+    }
 }
--- tests/aoc-2022-csharp-tests/Day06Tests.cs
+++ tests/aoc-2022-csharp-tests/Day06Tests.cs
@@ -4,11 +4,30 @@ namespace aoc_2022_csharp_tests;
 
 public class Day06Tests
 {
+    [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4, 7)]
+    [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 4, 5)]
+    [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 4, 6)]
+    [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4, 10)]
+    [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4, 11)]
+    [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14, 19)]
+    [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 14, 23)]
+    [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 14, 23)]
+    [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14, 29)]
+    [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14, 26)]
+    public void GetStartOfPacketMarkerTest(string message, int sliceSize, int expected)
+    {
+        // act
+        var actual = Day06.GetStartOfPacketMarker(message, sliceSize);
+
+        // assert
+        actual.Should().Be(expected);
+    }
+
     [Test]
     public void Part1Test()
     {
         // arrange
-        var expected = 0;
+        var expected = 1892;
 
         // act
         var actual = Day06.Part1();
@@ -21,7 +40,7 @@ public class Day06Tests
     public void Part2Test()
     {
         // arrange
-        var expected = 0;
+        var expected = 2313;
 
         // act
         var actual = Day06.Part2();
