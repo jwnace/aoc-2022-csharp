b756541ba430fddf0b17e74a0fdc325ab0c862ac
Joe Nace <jwnace@gmail.com>
2022-12-11 01:22:14 -0500

    using `long` instead of `int` fixed it... part 1 works now

--- src/aoc-2022-csharp/Day11/Day11.cs
+++ src/aoc-2022-csharp/Day11/Day11.cs
@@ -8,102 +8,60 @@ public static class Day11
     private class Monkey
     {
         public string Name { get; set; }
-        public int Number { get; set; }
-        public List<Item> Items { get; set; }
+        public long Number { get; set; }
+        public List<long> Items { get; set; }
         public string Operation { get; set; }
         public string Test { get; set; }
         public string IfTrue { get; set; }
         public string IfFalse { get; set; }
     }
 
-    private class Item
+    public static long Part1()
     {
-        public int Worry { get; set; }
-    }
+        var monkeys = GetMonkeys();
 
-    public static int Part1()
-    {
-        var monkeys = new List<Monkey>();
+        var dict = new Dictionary<long, long>
+            { { 0, 0 }, { 1, 0 }, { 2, 0 }, { 3, 0 }, { 4, 0 }, { 5, 0 }, { 6, 0 }, { 7, 0 }, };
 
-        foreach (var line in Input)
-        {
-            var values = line.Split(Environment.NewLine);
-            var name = values[0][..^1];
-            var number = int.Parse(values[0][^2].ToString());
-            var startingItems = values[1].Split(": ")[1].Split(", ").Select(int.Parse);
-            var operation = values[2].Split("= ")[1];
-            var test = values[3].Split(": ")[1];
-            var ifTrue = values[4].Split(": ")[1];
-            var ifFalse = values[5].Split(": ")[1];
-
-            monkeys.Add(new Monkey
-            {
-                Name = name,
-                Number = number,
-                Items = startingItems.Select(x => new Item { Worry = x }).ToList(),
-                Operation = operation,
-                Test = test,
-                IfTrue = ifTrue,
-                IfFalse = ifFalse
-            });
-        }
-
-        var dict = new Dictionary<int, int>
-        {
-            { 0, 0 },
-            { 1, 0 },
-            { 2, 0 },
-            { 3, 0 },
-            { 4, 0 },
-            { 5, 0 },
-            { 6, 0 },
-            { 7, 0 },
-        };
-
-        for (int round = 0; round < 20; round++)
+        for (var i = 0; i < 20; i++)
         {
             foreach (var monkey in monkeys)
             {
-                // var thrownItems = new List<Item>();
-
-                // for (var i = 0; i < monkey.Items.Count; i++)
                 while (monkey.Items.Count > 0)
                 {
                     dict[monkey.Number]++;
 
-                    var modifier = int.TryParse(monkey.Operation.Split(' ')[^1], out var value)
+                    var modifier = long.TryParse(monkey.Operation.Split(' ')[^1], out var value)
                         ? value
-                        : monkey.Items[0].Worry;
+                        : monkey.Items[0];
 
                     if (monkey.Operation.Contains("+"))
                     {
-                        monkey.Items[0].Worry += modifier;
+                        monkey.Items[0] += modifier;
                     }
                     else if (monkey.Operation.Contains("*"))
                     {
-                        monkey.Items[0].Worry *= modifier;
+                        monkey.Items[0] *= modifier;
                     }
                     else
                     {
                         throw new InvalidOperationException();
                     }
 
-                    monkey.Items[0].Worry /= 3;
+                    monkey.Items[0] /= 3;
 
-                    var divisor = int.Parse(monkey.Test.Split(' ')[^1]);
+                    var divisor = long.Parse(monkey.Test.Split(' ')[^1]);
 
-                    if (monkey.Items[0].Worry % divisor == 0)
+                    if (monkey.Items[0] % divisor == 0)
                     {
-                        var foo = int.Parse(monkey.IfTrue.Split(' ')[^1]);
-
-                        monkeys.Single(m => m.Number == foo).Items.Add(monkey.Items[0]);
+                        var number = long.Parse(monkey.IfTrue.Split(' ')[^1]);
+                        monkeys.Single(m => m.Number == number).Items.Add(monkey.Items[0]);
                         monkey.Items.RemoveAt(0);
                     }
                     else
                     {
-                        var foo = int.Parse(monkey.IfFalse.Split(' ')[^1]);
-
-                        monkeys.Single(m => m.Number == foo).Items.Add(monkey.Items[0]);
+                        var number = long.Parse(monkey.IfFalse.Split(' ')[^1]);
+                        monkeys.Single(m => m.Number == number).Items.Add(monkey.Items[0]);
                         monkey.Items.RemoveAt(0);
                     }
                 }
@@ -115,5 +73,35 @@ public static class Day11
         return topTwo[0] * topTwo[1];
     }
 
-    public static int Part2() => 2;
+    private static List<Monkey> GetMonkeys()
+    {
+        var monkeys = new List<Monkey>();
+
+        foreach (var line in Input)
+        {
+            var values = line.Split(Environment.NewLine);
+            var name = values[0][..^1];
+            var number = long.Parse(values[0][^2].ToString());
+            var startingItems = values[1].Split(": ")[1].Split(", ").Select(long.Parse).ToList();
+            var operation = values[2].Split("= ")[1];
+            var test = values[3].Split(": ")[1];
+            var ifTrue = values[4].Split(": ")[1];
+            var ifFalse = values[5].Split(": ")[1];
+
+            monkeys.Add(new Monkey
+            {
+                Name = name,
+                Number = number,
+                Items = startingItems,
+                Operation = operation,
+                Test = test,
+                IfTrue = ifTrue,
+                IfFalse = ifFalse
+            });
+        }
+
+        return monkeys;
+    }
+
+    public static long Part2() => 2;
 }
