22c5e52273105a302875887aa72d6afb66784126
Joe Nace <jwnace@gmail.com>
2022-12-13 08:12:52 -0500

    WIP - day 13

--- src/aoc-2022-csharp/Day13/Day13.cs
+++ src/aoc-2022-csharp/Day13/Day13.cs
@@ -1,10 +1,51 @@
-﻿namespace aoc_2022_csharp.Day13;
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace aoc_2022_csharp.Day13;
 
 public static class Day13
 {
-    private static readonly string[] Input = File.ReadAllLines("Day13/day13.txt");
+    private static readonly (string Left, string Right)[] Pairs = File.ReadAllText("Day13/day13.txt")
+        .Split($"{Environment.NewLine}{Environment.NewLine}")
+        .Select(p => p.Split(Environment.NewLine))
+        .Select(x => (x[0], x[1]))
+        .ToArray();
 
-    public static int Part1() => 1;
+    public static int Part1()
+    {
+        foreach (var pair in Pairs)
+        {
+            var left = JsonConvert.DeserializeObject(pair.Left);
+            var right = JsonConvert.DeserializeObject(pair.Right);
+
+            var areInCorrectOrder = AreInCorrectOrder(left, right);
+        }
+
+        return 1;
+    }
+
+    public static bool AreInCorrectOrder(object left, object right)
+    {
+        if (left is JArray l && right is JArray r)
+        {
+            for (int i = 0; i < l.Count; i++)
+            {
+                if (l[i] is JValue leftValue && r[i] is JValue rightValue)
+                {
+                    var foo = (int)leftValue;
+                    var bar = (int)rightValue;
+
+                    if (foo < bar)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
 
     public static int Part2() => 2;
 }
--- tests/aoc-2022-csharp-tests/Day13Tests.cs
+++ tests/aoc-2022-csharp-tests/Day13Tests.cs
@@ -1,32 +1,51 @@
 ﻿using aoc_2022_csharp.Day13;
+using Newtonsoft.Json;
 
 namespace aoc_2022_csharp_tests;
 
 public class Day13Tests
 {
-    [Test]
-    public void Part1Test()
+    [TestCase("[1,1,3,1,1]", "[1,1,5,1,1]", true)]
+    [TestCase("[[1],[2,3,4]]", "[[1],4]", true)]
+    [TestCase("[9]", "[[8,7,6]]", false)]
+    [TestCase("[[4,4],4,4]", "[[4,4],4,4,4]", true)]
+    [TestCase("[7,7,7,7]", "[7,7,7]", false)]
+    [TestCase("[]", "[3]", true)]
+    [TestCase("[[[]]]", "[[]]", false)]
+    [TestCase("[1,[2,[3,[4,[5,6,7]]]],8,9]", "[1,[2,[3,[4,[5,6,0]]]],8,9]", false)]
+    public void AreInCorrectOrderTest(string left, string right, bool expected)
     {
-        // arrange
-        var expected = 0;
-
         // act
-        var actual = Day13.Part1();
+        var actual = Day13.AreInCorrectOrder(JsonConvert.DeserializeObject(left), JsonConvert.DeserializeObject(right));
 
         // assert
         actual.Should().Be(expected);
     }
 
-    [Test]
-    public void Part2Test()
-    {
-        // arrange
-        var expected = 0;
 
-        // act
-        var actual = Day13.Part2();
-
-        // assert
-        actual.Should().Be(expected);
-    }
+    // [Test]
+    // public void Part1Test()
+    // {
+    //     // arrange
+    //     var expected = 0;
+    //
+    //     // act
+    //     var actual = Day13.Part1();
+    //
+    //     // assert
+    //     actual.Should().Be(expected);
+    // }
+    //
+    // [Test]
+    // public void Part2Test()
+    // {
+    //     // arrange
+    //     var expected = 0;
+    //
+    //     // act
+    //     var actual = Day13.Part2();
+    //
+    //     // assert
+    //     actual.Should().Be(expected);
+    // }
 }
