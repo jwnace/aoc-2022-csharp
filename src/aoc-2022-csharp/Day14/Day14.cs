b5207ecd5577f47603176fbd0e66d71ca6df3e24
Joe Nace <jwnace@gmail.com>
2022-12-14 09:22:05 -0500

    refactoring for day 14

--- src/aoc-2022-csharp/Day14/Day14.cs
+++ src/aoc-2022-csharp/Day14/Day14.cs
@@ -6,60 +6,20 @@ public static class Day14
 
     public static int Part1()
     {
-        var map = new Dictionary<(int X, int Y), char>();
-        map[(500, 0)] = '+';
-
-        foreach (var line in Input)
-        {
-            var points = line.Split(" -> ");
-
-            for (var i = 0; i < points.Length - 1; i++)
-            {
-                var start = points[i];
-                var end = points[i + 1];
-
-                var startValues = start.Split(',').Select(int.Parse).ToArray();
-                var endValues = end.Split(',').Select(int.Parse).ToArray();
-
-                var startX = new[] { startValues[0], endValues[0] }.Min();
-                var endX = new[] { startValues[0], endValues[0] }.Max();
-
-                var startY = new[] { startValues[1], endValues[1] }.Min();
-                var endY = new[] { startValues[1], endValues[1] }.Max();
-
-                for (var x = startX; x <= endX; x++)
-                {
-                    for (var y = startY; y <= endY; y++)
-                    {
-                        map[(x, y)] = '#';
-                    }
-                }
-            }
-        }
-
-        // if any sand ever reaches this Y position, it will fall forever
-        var bottomlessVoid = map.Max(n => n.Key.Y);
-
+        var map = BuildMap();
+        var threshold = map.Max(n => n.Key.Y);
         var count = 0;
 
-        // keep going until the first grain of sand reaches the bottomless void
         while (true)
         {
             var grain = (X: 500, Y: 0);
 
-
-            // keep moving the grain of sand until it comes to rest
-            while (TryMovePart1(map, grain, out var newPosition))
+            while (TryMove(map, grain, out var newPosition) && grain.Y < threshold)
             {
                 grain = newPosition;
-
-                if (grain.Y >= bottomlessVoid)
-                {
-                    break;
-                }
             }
 
-            if (grain.Y >= bottomlessVoid)
+            if (grain.Y >= threshold)
             {
                 break;
             }
@@ -68,63 +28,22 @@ public static class Day14
             count++;
         }
 
-        DrawMap(map);
         return count;
     }
 
     public static int Part2()
     {
-        var map = new Dictionary<(int X, int Y), char>();
-        map[(500, 0)] = '+';
-
-        foreach (var line in Input)
-        {
-            var points = line.Split(" -> ");
-
-            for (var i = 0; i < points.Length - 1; i++)
-            {
-                var start = points[i];
-                var end = points[i + 1];
-
-                var startValues = start.Split(',').Select(int.Parse).ToArray();
-                var endValues = end.Split(',').Select(int.Parse).ToArray();
-
-                var startX = new[] { startValues[0], endValues[0] }.Min();
-                var endX = new[] { startValues[0], endValues[0] }.Max();
-
-                var startY = new[] { startValues[1], endValues[1] }.Min();
-                var endY = new[] { startValues[1], endValues[1] }.Max();
-
-                for (var x = startX; x <= endX; x++)
-                {
-                    for (var y = startY; y <= endY; y++)
-                    {
-                        map[(x, y)] = '#';
-                    }
-                }
-            }
-        }
-
-        // if any sand ever reaches this Y position, it will fall forever
-        var bottomlessVoid = map.Max(n => n.Key.Y);
+        var map = BuildMap();
         var floor = map.Max(n => n.Key.Y) + 2;
-
         var count = 0;
 
-        // keep going until the first grain of sand reaches the bottomless void
         while (true)
         {
             var grain = (X: 500, Y: 0);
 
-            // keep moving the grain of sand until it comes to rest
-            while (TryMovePart2(map, floor, grain, out var newPosition))
+            while (TryMove(map, grain, out var newPosition, floor) && newPosition != (500, 0))
             {
                 grain = newPosition;
-
-                if (grain == (500, 0))
-                {
-                    break;
-                }
             }
 
             map[grain] = 'o';
@@ -136,21 +55,55 @@ public static class Day14
             }
         }
 
-        // DrawMap(map);
         return count;
     }
 
-    private static bool TryMovePart1(Dictionary<(int X, int Y), char> map, (int X, int Y) grain,
-        out (int X, int Y) newPosition)
+    private static Dictionary<(int X, int Y), char> BuildMap()
     {
-        // TODO: try to move the grain (down, down-left, down-right)
-        var n1 = (X: grain.X, Y: grain.Y + 1);
-        var n2 = (X: grain.X - 1, Y: grain.Y + 1);
-        var n3 = (X: grain.X + 1, Y: grain.Y + 1);
+        var map = new Dictionary<(int X, int Y), char> { [(500, 0)] = '+' };
 
-        var neighbors = new[] { n1, n2, n3 };
+        foreach (var line in Input)
+        {
+            var points = line.Split(" -> ");
 
-        var query = neighbors.Where(n => map.TryGetValue(n, out _) == false).ToList();
+            for (var i = 0; i < points.Length - 1; i++)
+            {
+                var start = points[i];
+                var end = points[i + 1];
+                var startValues = start.Split(',').Select(int.Parse).ToArray();
+                var endValues = end.Split(',').Select(int.Parse).ToArray();
+                var startX = new[] { startValues[0], endValues[0] }.Min();
+                var endX = new[] { startValues[0], endValues[0] }.Max();
+                var startY = new[] { startValues[1], endValues[1] }.Min();
+                var endY = new[] { startValues[1], endValues[1] }.Max();
+
+                for (var x = startX; x <= endX; x++)
+                {
+                    for (var y = startY; y <= endY; y++)
+                    {
+                        map[(x, y)] = '#';
+                    }
+                }
+            }
+        }
+
+        return map;
+    }
+
+    private static bool TryMove(
+        IReadOnlyDictionary<(int X, int Y), char> map,
+        (int X, int Y) grain,
+        out (int X, int Y) newPosition,
+        int floor = 0)
+    {
+        var neighbors = new[]
+        {
+            (X: grain.X    , Y: grain.Y + 1),
+            (X: grain.X - 1, Y: grain.Y + 1),
+            (X: grain.X + 1, Y: grain.Y + 1)
+        };
+
+        var query = neighbors.Where(n => (floor == 0 || n.Y < floor) && map.TryGetValue(n, out _) == false).ToList();
 
         if (!query.Any())
         {
@@ -162,28 +115,7 @@ public static class Day14
         return true;
     }
 
-    private static bool TryMovePart2(Dictionary<(int X, int Y), char> map, int floor, (int X, int Y) grain,
-        out (int X, int Y) newPosition)
-    {
-        var n1 = (X: grain.X, Y: grain.Y + 1);
-        var n2 = (X: grain.X - 1, Y: grain.Y + 1);
-        var n3 = (X: grain.X + 1, Y: grain.Y + 1);
-
-        var neighbors = new[] { n1, n2, n3 };
-
-        var query = neighbors.Where(n => n.Y < floor && map.TryGetValue(n, out _) == false).ToList();
-
-        if (!query.Any())
-        {
-            newPosition = grain;
-            return false;
-        }
-
-        newPosition = query.First();
-        return true;
-    }
-
-    private static void DrawMap(Dictionary<(int X, int Y), char> map)
+    private static void DrawMap(IReadOnlyDictionary<(int X, int Y), char> map)
     {
         var minY = map.Min(n => n.Key.Y);
         var maxY = map.Max(n => n.Key.Y);
