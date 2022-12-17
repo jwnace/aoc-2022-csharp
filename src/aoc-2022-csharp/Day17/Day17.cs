b4ef7b32a9207cb542720449e8180bb34c4cddef
Joe Nace <jwnace@gmail.com>
2022-12-17 10:47:41 -0500

    added placeholder for day 17 solution

---
+++ src/aoc-2022-csharp/Day17/Day17.cs
@@ -0,0 +1,10 @@
+namespace aoc_2022_csharp.Day17;
+
+public static class Day17
+{
+    private static readonly string[] Input = File.ReadAllLines("Day17/day17.txt");
+
+    public static int Part1() => 1;
+
+    public static int Part2() => 2;
+}
---
+++ tests/aoc-2022-csharp-tests/Day17Tests.cs
@@ -0,0 +1,32 @@
+using aoc_2022_csharp.Day17;
+
+namespace aoc_2022_csharp_tests;
+
+public class Day17Tests
+{
+    [Test]
+    public void Part1Test()
+    {
+        // arrange
+        var expected = 0;
+
+        // act
+        var actual = Day17.Part1();
+
+        // assert
+        actual.Should().Be(expected);
+    }
+
+    [Test]
+    public void Part2Test()
+    {
+        // arrange
+        var expected = 0;
+
+        // act
+        var actual = Day17.Part2();
+
+        // assert
+        actual.Should().Be(expected);
+    }
+}
