a079c4428d8e212c8456f3d1705ce723992a32b5
Joe Nace <jwnace@gmail.com>
2022-12-21 01:59:34 -0500

    added solution for day 21 part 1

--- src/aoc-2022-csharp/Day21/Day21.cs
+++ src/aoc-2022-csharp/Day21/Day21.cs
@@ -4,7 +4,87 @@ public static class Day21
 {
     private static readonly string[] Input = File.ReadAllLines("Day21/day21.txt");
 
-    public static int Part1() => 1;
+    public static long Part1()
+    {
+        var monkeys = GetMonkeys();
 
-    public static int Part2() => 2;
+        PopulateMonkeys(monkeys);
+
+        var root = monkeys.First(m => m.Name == "root");
+
+        return root.GetValue();
+    }
+
+    private static void PopulateMonkeys(List<Monkey> monkeys)
+    {
+        foreach (var line in Input)
+        {
+            var values = line.Split(null);
+            var name = values[0][..^1];
+            var monkey = monkeys.First(m => m.Name == name);
+
+            if (values.Length == 2)
+            {
+                var number = long.Parse(values[1]);
+                monkey.Number = number;
+            }
+            else if (values.Length == 4)
+            {
+                var a = values[1];
+                var operation = values[2];
+                var b = values[3];
+
+                monkey.Left = monkeys.First(m => m.Name == a);
+                monkey.Right = monkeys.First(m => m.Name == b);
+                monkey.Operation = operation;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+
+    private static List<Monkey> GetMonkeys()
+    {
+        var monkeys = new List<Monkey>();
+
+        foreach (var line in Input)
+        {
+            var values = line.Split(null);
+            var name = values[0][..^1];
+
+            monkeys.Add(new Monkey { Name = name });
+        }
+
+        return monkeys;
+    }
+
+    public static long Part2() => 2;
+
+    private class Monkey
+    {
+        public string Name { get; set; }
+        public long? Number { get; set; }
+        public string Operation { get; set; }
+        public Monkey Left { get; set; }
+        public Monkey Right { get; set; }
+
+        public long GetValue()
+        {
+            if (Number is not null)
+            {
+                return Number.Value;
+            }
+
+            return Operation switch
+            {
+                "+" => Left.GetValue() + Right.GetValue(),
+                "-" => Left.GetValue() - Right.GetValue(),
+                "*" => Left.GetValue() * Right.GetValue(),
+                "/" => Left.GetValue() / Right.GetValue(),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+    }
 }
