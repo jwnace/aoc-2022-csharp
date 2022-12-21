8bd3b9764c12e07b890fb1dd8ef0e875345f14a9
Joe Nace <jwnace@gmail.com>
2022-12-21 01:40:46 -0500

    added placeholder for day 21 solution

---
+++ src/aoc-2022-csharp/Day21/Day21.cs
@@ -0,0 +1,10 @@
+﻿namespace aoc_2022_csharp.Day21;
+
+public static class Day21
+{
+    private static readonly string[] Input = File.ReadAllLines("Day21/day21.txt");
+
+    public static int Part1() => 1;
+
+    public static int Part2() => 2;
+}
