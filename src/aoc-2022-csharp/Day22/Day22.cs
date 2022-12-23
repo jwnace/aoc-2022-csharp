c919f7a2869a16b536236a352006fc10e499bbd1
Joe Nace <jwnace@gmail.com>
2022-12-23 00:45:32 -0500

    hacky, brute force solution for day 22... but it works

--- src/aoc-2022-csharp/Day22/Day22.cs
+++ src/aoc-2022-csharp/Day22/Day22.cs
@@ -35,7 +35,34 @@ public static class Day22
         return (1000 * row) + (4 * col) + (int)facing;
     }
 
-    public static int Part2() => 2;
+    public static int Part2()
+    {
+        var values = Input.Split($"{Environment.NewLine}{Environment.NewLine}");
+        var map = GetMap(values[0]);
+        var instructions = GetInstructions(values[1]);
+
+        var startingRow = map.Min(m => m.Key.Row);
+        var startingCol = map.Where(m => m.Key.Row == startingRow).Min(m => m.Key.Col);
+
+        var state = (Row: startingRow, Col: startingCol, Facing: Facing.Right);
+        map[(state.Row, state.Col)] = '>';
+
+        foreach (var instruction in instructions)
+        {
+            if (instruction is int steps)
+            {
+                MovePart2(ref state, map, steps);
+            }
+
+            if (instruction is char turn)
+            {
+                Turn(ref state, map, turn);
+            }
+        }
+
+        var (row, col, facing) = state;
+        return (1000 * row) + (4 * col) + (int)facing;
+    }
 
     private static Dictionary<(int Row, int Col), char> GetMap(string input)
     {
@@ -121,7 +148,35 @@ public static class Day22
         state = (row, col, facing);
     }
 
-    private static bool TryMove(ref (int Row, int Col, Facing Facing) state, Dictionary<(int Row, int Col), char> map)
+    private static void MovePart2(ref (int, int, Facing) state, Dictionary<(int Row, int Col), char> map, int steps)
+    {
+        var (row, col, facing) = state;
+
+        for (var i = 0; i < steps; i++)
+        {
+            if (!TryMovePart2(ref state, map))
+            {
+                break;
+            }
+
+            // HACK: update row, col, and facing since they may have been changed by the above code
+            // TODO: instead of using tuples everywhere, I should make a private record or class to track the state
+            (row, col, facing) = state;
+
+            map[(row, col)] = facing switch
+            {
+                Facing.Right => '>',
+                Facing.Left => '<',
+                Facing.Up => '^',
+                Facing.Down => 'v',
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        state = (row, col, facing);
+    }
+
+    private static bool TryMove(ref (int, int, Facing) state, Dictionary<(int Row, int Col), char> map)
     {
         var (row, col, facing) = state;
 
@@ -173,6 +228,146 @@ public static class Day22
         }
 
         state = (temp.Row, temp.Col, facing);
+        return true;
+    }
+
+    private static bool TryMovePart2(ref (int, int, Facing) state, Dictionary<(int Row, int Col), char> map)
+    {
+        var (row, col, facing) = state;
+
+        var temp = facing switch
+        {
+            Facing.Right => (Row: row, Col: col + 1, Facing: facing),
+            Facing.Left => (Row: row, Col: col - 1, Facing: facing),
+            Facing.Up => (Row: row - 1, Col: col, Facing: facing),
+            Facing.Down => (Row: row + 1, Col: col, Facing: facing),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        if (map.ContainsKey((temp.Row, temp.Col)))
+        {
+            if (map[(temp.Row, temp.Col)] == '#')
+            {
+                return false;
+            }
+
+            state = temp;
+            return true;
+        }
+
+        // we went over an edge, and we need to wrap
+        switch (facing)
+        {
+            case Facing.Right when (row - 1) / 50 == 0:
+            {
+                var tempRow = 100 + (50 - row + 1);
+                var tempCol = map.Where(m => m.Key.Row == tempRow).Max(m => m.Key.Col);
+                temp = (tempRow, tempCol, Facing.Left);
+                break;
+            }
+            case Facing.Right when (row - 1) / 50 == 1:
+            {
+                var tempCol = 100 + (row - 50);
+                var tempRow = map.Where(m => m.Key.Col == tempCol).Max(m => m.Key.Row);
+                temp = (tempRow, tempCol, Facing.Up);
+                break;
+            }
+            case Facing.Right when (row - 1) / 50 == 2:
+            {
+                var tempRow = 0 + (150 - row + 1);
+                var tempCol = map.Where(m => m.Key.Row == tempRow).Max(m => m.Key.Col);
+                temp = (tempRow, tempCol, Facing.Left);
+                break;
+            }
+            case Facing.Right when (row - 1) / 50 == 3:
+            {
+                var tempCol = 50 + (row - 150);
+                var tempRow = map.Where(m => m.Key.Col == tempCol).Max(m => m.Key.Row);
+                temp = (tempRow, tempCol, Facing.Up);
+                break;
+            }
+            case Facing.Left when (row - 1) / 50 == 0:
+            {
+                var tempRow = 100 + (50 - row + 1);
+                var tempCol = map.Where(m => m.Key.Row == tempRow).Min(m => m.Key.Col);
+                temp = (tempRow, tempCol, Facing.Right);
+                break;
+            }
+            case Facing.Left when (row - 1) / 50 == 1:
+            {
+                var tempCol = row - 50;
+                var tempRow = map.Where(m => m.Key.Col == tempCol).Min(m => m.Key.Row);
+                temp = (tempRow, tempCol, Facing.Down);
+                break;
+            }
+            case Facing.Left when (row - 1) / 50 == 2:
+            {
+                var tempRow = 150 - row + 1;
+                var tempCol = map.Where(m => m.Key.Row == tempRow).Min(m => m.Key.Col);
+                temp = (tempRow, tempCol, Facing.Right);
+                break;
+            }
+            case Facing.Left when (row - 1) / 50 == 3:
+            {
+                var tempCol = 50 + (row - 150);
+                var tempRow = map.Where(m => m.Key.Col == tempCol).Min(m => m.Key.Row);
+                temp = (tempRow, tempCol, Facing.Down);
+                break;
+            }
+            case Facing.Up when (col - 1) / 50 == 0:
+            {
+                var tempRow = 50 + col;
+                var tempCol = map.Where(m => m.Key.Row == tempRow).Min(m => m.Key.Col);
+                temp = (tempRow, tempCol, Facing.Right);
+                break;
+            }
+            case Facing.Up when (col - 1) / 50 == 1:
+            {
+                var tempRow = 150 + col - 50;
+                var tempCol = map.Where(m => m.Key.Row == tempRow).Min(m => m.Key.Col);
+                temp = (tempRow, tempCol, Facing.Right);
+                break;
+            }
+            case Facing.Up when (col - 1) / 50 == 2:
+            {
+                var tempCol = col - 100;
+                var tempRow = map.Where(m => m.Key.Col == tempCol).Max(m => m.Key.Row);
+                temp = (tempRow, tempCol, Facing.Up);
+                break;
+            }
+            case Facing.Down when (col - 1) / 50 == 0:
+            {
+                var tempCol = 100 + col;
+                var tempRow = map.Where(m => m.Key.Col == tempCol).Min(m => m.Key.Row);
+                temp = (tempRow, tempCol, Facing.Down);
+                break;
+            }
+            case Facing.Down when (col - 1) / 50 == 1:
+            {
+                var tempRow = 150 + col - 50;
+                var tempCol = map.Where(m => m.Key.Row == tempRow).Max(m => m.Key.Col);
+                temp = (tempRow, tempCol, Facing.Left);
+                break;
+            }
+            case Facing.Down when (col - 1) / 50 == 2:
+            {
+                var tempRow = 50 + col - 100;
+                var tempCol = map.Where(m => m.Key.Row == tempRow).Max(m => m.Key.Col);
+                temp = (tempRow, tempCol, Facing.Left);
+                break;
+            }
+            default:
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        if (map[(temp.Row, temp.Col)] == '#')
+        {
+            return false;
+        }
+
+        state = (temp.Row, temp.Col, temp.Facing);
         return true;
     }
 
