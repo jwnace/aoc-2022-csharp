05140b962aa2c28f2baa377c427d41413ee460d3
Joe Nace <jwnace@gmail.com>
2022-12-23 01:00:29 -0500

    added placeholder for day 23 solution

---
+++ src/aoc-2022-csharp/Day23/Day23.cs
@@ -0,0 +1,10 @@
+﻿namespace aoc_2022_csharp.Day23;
+
+public static class Day23
+{
+    private static readonly string[] Input = File.ReadAllLines("Day23/day23.txt");
+
+    public static int Part1() => 1;
+
+    public static int Part2() => 2;
+}
--- src/aoc-2022-csharp/Program.cs
+++ src/aoc-2022-csharp/Program.cs
@@ -40,5 +40,7 @@
 // Console.WriteLine($"Day 20, Part 2: {Day20.Part2()}");
 // Console.WriteLine($"Day 21, Part 1: {Day21.Part1()}");
 // Console.WriteLine($"Day 21, Part 2: {Day21.Part2()}");
-Console.WriteLine($"Day 22, Part 1: {Day22.Part1()}");
-Console.WriteLine($"Day 22, Part 2: {Day22.Part2()}");
+// Console.WriteLine($"Day 22, Part 1: {Day22.Part1()}");
+// Console.WriteLine($"Day 22, Part 2: {Day22.Part2()}");
+Console.WriteLine($"Day 23, Part 1: {Day23.Part1()}");
+Console.WriteLine($"Day 23, Part 2: {Day23.Part2()}");
