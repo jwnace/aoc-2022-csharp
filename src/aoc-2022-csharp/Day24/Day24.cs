71ff4b894843eea82ec381f8a57d7019dd21d681
Joe Nace <jwnace@gmail.com>
2022-12-24 11:04:40 -0500

    WIP day 24, I think this solution will get the right answer, but it takes forever to run

--- src/aoc-2022-csharp/Day24/Day24.cs
+++ src/aoc-2022-csharp/Day24/Day24.cs
@@ -1,71 +1,96 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.Diagnostics.Contracts;
 
 namespace aoc_2022_csharp.Day24;
 
 public static class Day24
 {
     private static readonly string[] Input = File.ReadAllLines("Day24/day24.txt");
+    private static readonly Dictionary<(int, int), char> Map = GetMap();
+    private static readonly List<Blizzard> Blizzards = GetBlizzards(Map);
+    private static readonly Dictionary<int, List<Blizzard>> BlizzardMemo = new();
 
     private static Dictionary<(Dictionary<(int, int), char>, List<Blizzard>, (int, int)), int> Memo = new();
 
     public static int Part1()
     {
-        var map = GetMap();
-        var blizzards = GetBlizzards(map);
-        var player = (Row: 0, Col: 1);
+        // HACK: I need to do this... but I would like to find a better way to do it...
+        // maybe I can encapsulate this inside of GetBlizzards()
+        RemoveBlizzardsFromMap(Map, Blizzards);
 
-        RemoveBlizzardsFromMap(map, blizzards);
-        // DrawMap(map, blizzards, player);
-
-        return GetShortestPath((map, blizzards, player));
+        return GetShortestPath(((Row: 0, Col: 1), Time: 0));
     }
 
-    private static int GetShortestPath((
-        Dictionary<(int Row, int Col), char> Map,
-        List<Blizzard> Blizzards,
-        (int Row, int Col) Player) initialState)
+    private static int GetShortestPath(((int Row, int Col) Player, int Time) initialState)
     {
-        var queue = new Queue<(
-            Dictionary<(int Row, int Col), char> map,
-            List<Blizzard> blizzards,
-            (int Row, int Col) player)>();
-
-        queue.Enqueue(initialState);
-
-        var distances = new Dictionary<(
-            Dictionary<(int Row, int Col), char> map,
-            List<Blizzard> blizzards,
-            (int Row, int Col) player), int>();
-
+        var distances = new Dictionary<((int Row, int Col) player, int time), int>();
         distances[initialState] = 0;
 
-        // HACK: I think I need to move the blizzards ahead of time because I'm computing the NEXT state...
-        // not sure if that makes sense or not
-        // MoveBlizzards(initialState.Map, initialState.Blizzards);
+        var queue = new PriorityQueue<((int Row, int Col) Player, int Time), int>();
+        queue.Enqueue(initialState, 0);
 
-        while (queue.Any())
+        var seen = new HashSet<((int Row, int Col) player, int time)>();
+
+        while (queue.Count > 0)
         {
             var state = queue.Dequeue();
-            var (map, blizzards, player) = state;
+            var (player, time) = state;
 
-            var minRow = map.Min(m => m.Key.Row);
-            var maxRow = map.Max(m => m.Key.Row);
-            var minCol = map.Min(m => m.Key.Col);
-            var maxCol = map.Max(m => m.Key.Col);
+            if (seen.Contains(state))
+            {
+                continue;
+            }
+
+            seen.Add(state);
+
+            if (seen.Count % 100 == 0)
+            {
+                Console.WriteLine($"seen: {seen.Count}, distance: {distances[state]}, time: {state.Time}");
+            }
+
+            var minRow = Map.Min(m => m.Key.Item1);
+            var maxRow = Map.Max(m => m.Key.Item1);
+            var minCol = Map.Min(m => m.Key.Item2);
+            var maxCol = Map.Max(m => m.Key.Item2);
             var destination = (maxRow, maxCol - 1);
 
             if (player == destination)
             {
-                DrawMap(map, blizzards, player);
+                // DrawMap(Map, blizzards, player);
+
+                Console.WriteLine($"seen: {seen.Count}, distance: {distances[state]}, time: {state.Time}");
+
+                // var query = distances.Select(x => (x.Key.time, x.Key.player.Row, x.Key.player.Col))
+                //     .OrderBy(x => x.time)
+                //     .ThenBy(x => x.Row)
+                //     .ThenBy(x => x.Col)
+                //     .Select(x => $"{x.time} => ({x.Row}, {x.Col})")
+                //     .ToList();
+
+                DrawMap(Map, GetBlizzards(time), player);
+
                 return distances[state];
             }
 
-            if (blizzards.Any(b => b.Position == player))
+            // this is actually fine... we just moved this blizzard on top of the player,
+            // and they're immediately going to move out of it
+            // if (blizzards.Any(b => b.Position == player))
+            // {
+            //     throw new Exception("The player is in a blizzard!");
+            // }
+
+            // get the blizzards for the NEXT minute, to determine where it will be safe to move
+            var blizzards = GetBlizzards(time + 1);
+
+            if (state.Time == 4)
             {
-                throw new Exception("The player is in a blizzard!");
+                // DrawMap(Map, blizzards, player);
             }
 
-            MoveBlizzards(map, blizzards);
+            if (state.Time == 4 && state.Player == (1, 1))
+            {
+                // DrawMap(Map, blizzards, player);
+                var foo = "break here!";
+            }
 
             var (row, col) = player;
 
@@ -77,21 +102,23 @@ public static class Day24
                 (Row: row, Col: col - 1),
             };
 
+            var playerWasAbleToMove = false;
+
             foreach (var neighbor in neighbors)
             {
                 // don't move into a wall
-                if (map.TryGetValue(neighbor, out var value) && value == '#')
+                if (Map.TryGetValue(neighbor, out var value) && value == '#')
                 {
                     continue;
                 }
 
-                // don't move above the map
+                // don't move above the Map
                 if (neighbor.Row < minRow)
                 {
                     continue;
                 }
 
-                // don't move below the map
+                // don't move below the Map
                 if (neighbor.Row > maxRow)
                 {
                     continue;
@@ -103,25 +130,59 @@ public static class Day24
                     continue;
                 }
 
-                distances[(map, blizzards, neighbor)] = distances[state] + 1;
-                queue.Enqueue((map, blizzards, neighbor));
+                var newState = (neighbor, time + 1);
+                // var distance = distances.TryGetValue(newState, out var v) ? v : int.MaxValue;
+                // distance = Math.Min(distance, distances[state] + 1);
+
+                var distance = distances[state] + 1;
+
+                distances[newState] = distance;
+                queue.Enqueue(newState, distance);
+
+                playerWasAbleToMove = true;
             }
 
-            // don't move at all
-            distances[(map, blizzards, player)] = distances[state] + 1;
-            queue.Enqueue((map, blizzards, player));
+            // if a blizzard moved on top of the player, the player MUST move
+            // if the player was able to move, they should move and not stay in the same place
+            if (blizzards.All(b => b.Position != player) && !playerWasAbleToMove)
+            {
+                var newState = (player, time + 1);
+                // var distance = distances.TryGetValue(newState, out var v) ? v : int.MaxValue;
+                // distance = Math.Min(distance, distances[state] + 1);
+
+                var distance = distances[state] + 1;
+                // var timesSeen = seenTimes.TryGetValue(newState, out var times) ? times : 0;
+
+                // HACK: if we've seen this state more than N times, we don't want to consider it anymore
+                // if (seen.Contains(newState) && timesSeen < 500)
+                // {
+                // HACK: if I'm not moving, it's going to look like the same state again, so remove it from `seen`
+                // seen.Remove(newState);
+                // seenTimes[newState] = timesSeen + 1;
+                // }
+
+                // don't move at all
+                distances[newState] = distance;
+                queue.Enqueue(newState, distance);
+            }
+            // else
+            // {
+            //     throw new Exception("something went horribly wrong");
+            // }
         }
 
         return 0;
     }
 
-    private static void MoveBlizzards(Dictionary<(int Row, int Col), char> map, List<Blizzard> blizzards)
+    private static IEnumerable<Blizzard> MoveBlizzards(Dictionary<(int Row, int Col), char> map, IEnumerable<Blizzard> blizzards)
     {
+        var result = new List<Blizzard>();
+
         foreach (var blizzard in blizzards)
         {
             var (row, col, direction) = blizzard;
 
-            blizzard.Position = direction switch
+            var position = direction switch
             {
                 '^' => (row - 1, col),
                 'v' => (row + 1, col),
@@ -130,14 +191,14 @@ public static class Day24
                 _ => throw new ArgumentOutOfRangeException()
             };
 
-            if (map.TryGetValue(blizzard.Position, out var value) && value == '#')
+            if (map.TryGetValue(position, out var value) && value == '#')
             {
                 var minRow = map.Min(n => n.Key.Row);
                 var maxRow = map.Max(n => n.Key.Row);
                 var minCol = map.Min(n => n.Key.Col);
                 var maxCol = map.Max(n => n.Key.Col);
 
-                blizzard.Position = direction switch
+                position = direction switch
                 {
                     '^' => (maxRow - 1, col),
                     'v' => (minRow + 1, col),
@@ -146,6 +207,8 @@ public static class Day24
                     _ => throw new ArgumentOutOfRangeException()
                 };
             }
+
+            yield return new Blizzard(position, blizzard.Direction);
         }
     }
 
@@ -167,6 +230,33 @@ public static class Day24
             .Where(x => x.Value is '^' or 'v' or '<' or '>')
             .Select(x => new Blizzard((x.Key.Row, x.Key.Col), x.Value))
             .ToList();
+
+        return blizzards;
+    }
+
+    private static List<Blizzard> GetBlizzards(int time)
+    {
+        if (BlizzardMemo.TryGetValue(time, out var value))
+        {
+            return value;
+        }
+
+        var start = 0;
+
+        var blizzards = Blizzards;
+
+        if (BlizzardMemo.Any())
+        {
+            // start from the closest value
+            start = BlizzardMemo.Max(x => x.Key);
+            blizzards = BlizzardMemo[start];
+        }
+
+        for (var i = start; i < time; i++)
+        {
+            blizzards = MoveBlizzards(Map, blizzards).ToList();
+        }
+
         return blizzards;
     }
 
@@ -210,22 +300,21 @@ public static class Day24
                 {
                     var b = blizzards.Where(b => b.Position == (row, col)).ToList();
 
-                    if (player == (row, col) && b.Any())
-                    {
-                        throw new Exception("The player is in a blizzard!");
-                    }
-
-                    if (player == (row, col))
+                    // if (player == (row, col) && b.Any())
+                    // {
+                        // value = '!';
+                        // throw new Exception("The player is in a blizzard!");
+                    // }
+                    if (player == (row, col) && !b.Any())
                     {
                         value = 'E';
+                        // value = '.';
                     }
-
-                    if (b.Count == 1)
+                    else if (b.Count == 1)
                     {
                         value = b.First().Direction;
                     }
-
-                    if (b.Count > 1)
+                    else if (b.Count > 1)
                     {
                         value = b.Count < 10 ? char.Parse(b.Count.ToString()) : '*';
                     }
